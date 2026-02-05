b817045c9029002d9183e5d7351a027315054bab
shibahex <badamsva@gmail.com>
2026-02-04 22:24:52 -0500

    fix: update neovim config!

--- modules/home-apps/neovim.nix
+++ modules/home-apps/neovim.nix
@@ -5,7 +5,7 @@ let
     owner = "shibahex";
     repo = "kickstart.nvim";
     sha256 = "1sbbdxrnsc67zqw6qkm2mahvlp6hvbld4rxbm2zqixf2diccb638";
-    rev = "74292b85a2770545d1e5e17fcc2a903708af26d5";
+    rev = "e3263d9cecaf27e7da88107ae23d58149827e9de";
   };
 in
 {
