231e2c123b15351fec680d71ba14ed523a653c6e
shibahex <badamsva@gmail.com>
2026-02-27 18:00:01 -0500

    desktop: add more packages

--- hosts/nixos-desktop/host-packages.nix
+++ hosts/nixos-desktop/host-packages.nix
@@ -1,4 +1,5 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+{
   environment.systemPackages = with pkgs; [
     # Add host-specific packages here
     (anki.withAddons [ ankiAddons.review-heatmap ])
@@ -8,13 +9,20 @@
 
     looking-glass-client
     freerdp
+    mangohud
+
+    mpv
+    tmux
+    openvpn
   ];
 
   # for looking glass
   systemd.tmpfiles.rules = [ "f /dev/shm/looking-glass 0660 $USER kvm -" ];
 
+  programs.gamemode.enable = true;
   virtualisation.docker.enable = true;
 
+  powerManagement.cpuFreqGovernor = "performance";
   # services.wazuh-agent = {
   #   enable = true;
   #   # Add your Wazuh manager address
