5ec5258ed3733df7eb0050f01083d25c293ce064
shibahex <badamsva@gmail.com>
2026-02-27 18:00:10 -0500

    desktop: organize monitors

--- hosts/nixos-desktop/variables.nix
+++ hosts/nixos-desktop/variables.nix
@@ -2,7 +2,8 @@
 let
   mainMonitor = "DP-0";
   sideMonitorRight = "DP-2";
-in {
+in
+{
   #Mainly used for hardware.nix or etc, inital setup in flake.nix has your hostname
   hostName = "shiba";
 
@@ -21,17 +22,25 @@ in {
 
   defaultShell = "nushell";
 
-  monitorRules = [{
-    name = "Gaming monitors";
-    outputs_connected = [ mainMonitor sideMonitorRight ];
-    configure_row = [ mainMonitor sideMonitorRight ];
-    primary = mainMonitor;
-    atomic = true;
-    execute_after = [
-      "${pkgs.xorg.xrandr}/bin/xrandr --output ${mainMonitor} --mode 3840x2160 --rate 144 --dpi 192 --primary"
-      "${pkgs.xorg.xrandr}/bin/xrandr --output ${sideMonitorRight} --mode 2560x1440 --rate 100 --rotate normal" # --rotate right"
-    ];
-  }];
+  monitorRules = [
+    {
+      name = "Gaming monitors";
+      outputs_connected = [
+        mainMonitor
+        sideMonitorRight
+      ];
+      configure_row = [
+        mainMonitor
+        sideMonitorRight
+      ];
+      primary = mainMonitor;
+      atomic = true;
+      execute_after = [
+        "${pkgs.xorg.xrandr}/bin/xrandr --output ${mainMonitor} --mode 3840x2160 --rate 144 --dpi 192 --primary"
+        "${pkgs.xorg.xrandr}/bin/xrandr --output ${sideMonitorRight} --mode 2560x1440 --rate 100 --rotate normal" # --rotate right"
+      ];
+    }
+  ];
 
   # Set network hostId if required (needed for zfs)
   hostId = "5ab03f50";
