8c85de5d8f709666083e8a3318da239ac63a8036
shibahex <badamsva@gmail.com>
2026-02-27 18:01:48 -0500

    enable audio for nvidia drivers

--- modules/drivers/nvidia-drivers.nix
+++ modules/drivers/nvidia-drivers.nix
@@ -1,17 +1,8 @@
-{
-  lib,
-  pkgs,
-  config,
-  ...
-}:
+{ lib, pkgs, config, ... }:
 with lib;
-let
-  cfg = config.drivers.nvidia;
-in
-{
-  options.drivers.nvidia = {
-    enable = mkEnableOption "Enable Nvidia Drivers";
-  };
+let cfg = config.drivers.nvidia;
+in {
+  options.drivers.nvidia = { enable = mkEnableOption "Enable Nvidia Drivers"; };
   config = mkIf cfg.enable {
     services.xserver.videoDrivers = [ "nvidia" ];
     hardware.nvidia = {
@@ -36,5 +27,11 @@ in
       # Optionally, you may need to select the appropriate driver version for your specific GPU.
       package = config.boot.kernelPackages.nvidiaPackages.latest;
     };
+    hardware.graphics = {
+      enable = true;
+      enable32Bit = true;
+      extraPackages = with pkgs; [ nvidia-vaapi-driver libvdpau-va-gl ];
+    };
+
   };
 }
