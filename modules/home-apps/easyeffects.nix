a4daede1d9fd2ec38bb03645970be5ef2edd91f1
shibahex <badamsva@gmail.com>
2026-03-02 00:08:16 -0500

    fix easyeffects hash

--- modules/home-apps/easyeffects.nix
+++ modules/home-apps/easyeffects.nix
@@ -1,12 +1,13 @@
 # The goal of this module is to add easyeffects presets from git into the .config file
 { pkgs, ... }:
 let
-  easyEffectsProfiles = pkgs.fetchFromGitHub {
-    owner = "shibahex";
-    repo = "easyeffects-presets";
-    sha256 = "1q22z0q0pbp5j67aahxaf6byjfrzsv39szzbn7h3vmf77g10nb0v";
-    rev = "4881e625b4debbfc9fc007f05c10184e0bfff3ee";
-  };
+  easyEffectsProfiles = pkgs.fetchFromGitHub
+{
+    "owner": "shibahex",
+    "repo": "easyeffects-presets",
+    "rev": "4881e625b4debbfc9fc007f05c10184e0bfff3ee",
+    "hash": "sha256-cWQBVPbB+/VrYjid+qJ+yord6gxfJ7DGZCoPpaBuQXY="
+};
 in
 {
   home.file.".config/easyeffects" = {
