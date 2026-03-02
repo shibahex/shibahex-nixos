8352da3c0a4c3cdf56b2b226257d74f523c6401c
shibahex <badamsva@gmail.com>
2026-03-02 00:10:41 -0500

    fix: fix fetchfromGithub format

--- modules/home-apps/easyeffects.nix
+++ modules/home-apps/easyeffects.nix
@@ -1,13 +1,12 @@
 # The goal of this module is to add easyeffects presets from git into the .config file
 { pkgs, ... }:
 let
-  easyEffectsProfiles = pkgs.fetchFromGitHub
-{
-    "owner": "shibahex",
-    "repo": "easyeffects-presets",
-    "rev": "4881e625b4debbfc9fc007f05c10184e0bfff3ee",
-    "hash": "sha256-cWQBVPbB+/VrYjid+qJ+yord6gxfJ7DGZCoPpaBuQXY="
-};
+  easyEffectsProfiles = pkgs.fetchFromGitHub {
+    "owner" = "shibahex";
+    "repo" = "easyeffects-presets";
+    "rev" = "4881e625b4debbfc9fc007f05c10184e0bfff3ee";
+    "hash" = "sha256-cWQBVPbB+/VrYjid+qJ+yord6gxfJ7DGZCoPpaBuQXY=";
+  };
 in
 {
   home.file.".config/easyeffects" = {
