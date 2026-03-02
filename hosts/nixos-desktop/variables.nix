4291a3c01d2f7dc6e123f2765da701eaf488ad7c
shibahex <badamsva@gmail.com>
2026-03-01 23:03:55 -0500

    refactor: remove non important vars

--- hosts/nixos-desktop/variables.nix
+++ hosts/nixos-desktop/variables.nix
@@ -2,23 +2,21 @@
 let
   mainMonitor = "DP-0";
   sideMonitorRight = "DP-2";
+
 in
 {
   #Mainly used for hardware.nix or etc, inital setup in flake.nix has your hostname
   hostName = "shiba";
 
   # Git Configuration
-  gitUsername = "shiba";
-  gitEmail = "shiba@nixos-desktop";
   timeZone = "America/New_York";
 
   # For Nvidia Prime support
   # Run 'lspci | grep VGA' to find your actual GPU IDs
   nvidiaID = "PCI:1:0:0";
 
+  # GPU passthrough
   vfioIds = "10de:1b06,10de:10ef";
-  # Startup Applications
-  startupApps = [ ];
 
   defaultShell = "nushell";
 
