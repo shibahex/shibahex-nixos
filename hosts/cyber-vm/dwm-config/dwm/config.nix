8c97aa53a0b1fcd4f0af53000117c438bf1d35a3
shibahex <badamsva@gmail.com>
2026-03-02 20:18:09 -0500

    cyber-vm: switch to alacritty

--- hosts/cyber-vm/dwm-config/dwm/config.nix
+++ hosts/cyber-vm/dwm-config/dwm/config.nix
@@ -63,7 +63,7 @@
     //static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon};//, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
     static const char *dmenucmd[] = { "env", "SHELL=/bin/sh", "dmenu_run", "-m", dmenumon, NULL };
 
-    static const char *termcmd[]  = { "st", NULL };
+    static const char *termcmd[]  = { "alacritty", NULL };
 
     static const Key keys[] = {
     	/* modifier                     key        function        argument */
--- hosts/cyber-vm/host-packages.nix
+++ hosts/cyber-vm/host-packages.nix
@@ -7,6 +7,7 @@
     btop
 
     mpv
+    alacritty
     tmux
     openvpn
 
