484229cee7005d7084782bdb7d3aaac760d72317
shibahex <badamsva@gmail.com>
2026-03-05 11:37:26 -0500

    change: switch back to ly, less problems.

--- modules/core/boot.nix
+++ modules/core/boot.nix
@@ -19,6 +19,6 @@
       mask = "\\xff\\xff\\xff\\xff\\x00\\x00\\x00\\x00\\xff\\xff\\xff";
       magicOrExtension = "\\x7fELF....AI\\x02";
     };
-    plymouth.enable = true;
+   #plymouth.enable = true;
   };
 }
--- modules/desktop/default.nix
+++ modules/desktop/default.nix
@@ -18,27 +18,6 @@ in
     (lib.optional (builtins.elem "dwm" desktops) ./dwm-setup.nix)
     ++ (lib.optional (builtins.elem "niri" desktops) ./niri-setup.nix);
 
-  services.greetd = {
-    enable = true;
-    settings = {
-      default_session = {
-        # Cage extends the screen
-        command = "${pkgs.cage}/bin/cage -s -- ${pkgs.tuigreet}/bin/tuigreet \
-          --time \
-          --asterisks
-          --sessions ${config.services.displayManager.sessionData.desktops}/share/xsessions:${config.services.displayManager.sessionData.desktops}/share/wayland-sessions";
-        user = "greeter";
-      };
-    };
-  };
-
-  systemd.services.greetd.serviceConfig = {
-    Type = "idle";
-    StandardInput = "tty";
-    StandardOutput = "tty";
-    StandardError = "journal";
-    TTYReset = true;
-    TTYVHangup = true;
-    TTYVTDisallocate = true;
-  };
+   services.displayManager.ly.enable = true;
+ 
 }
