412dae87fd59cc188b8f38908f21c5436f458f2a
shibahex <badamsva@gmail.com>
2026-03-06 03:26:52 -0500

    change: reformat all my drives to btrfs and create subvols

--- hosts/nixos-desktop/host-packages.nix
+++ hosts/nixos-desktop/host-packages.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 {
   environment.systemPackages = with pkgs; [
     # Add host-specific packages here
@@ -6,47 +6,36 @@
     # For timing tasks and productivity
     timer
     btop
-
     looking-glass-client
     freerdp
     mangohud
-
     mpv
     tmux
     openvpn
-
-    # Recording App
-    obs-studio
-
     # Discord
     vesktop
-
     # Extra browser
     librewolf
-
     #Yubikey
     yubikey-manager
-
     linuxPackages.usbip
-
     fastfetch
-
     thunar
     ffmpegthumbnailer
-
+    
+    # Recording (make OBS see nvidiaPackages)
+    (pkgs.writeShellScriptBin "obs" ''
+      export LD_LIBRARY_PATH=${config.boot.kernelPackages.nvidiaPackages.stable}/lib:$LD_LIBRARY_PATH
+      exec ${pkgs.obs-studio}/bin/obs "$@"
+    '')
   ];
-
   # ffmpegthumbnailer and tumbler for mp4 thumbnails
   services.tumbler.enable = true;
-
   # for looking glass
   systemd.tmpfiles.rules = [ "f /dev/shm/looking-glass 0660 $USER kvm -" ];
-
   programs.gamemode.enable = true;
   virtualisation.docker.enable = true;
-
   powerManagement.cpuFreqGovernor = "performance";
-
   # USB Yubikey to VM. (Can be insecure if public)
   systemd.services.usbipd = {
     enable = true;
@@ -57,15 +46,12 @@
       Restart = "on-failure";
     };
   };
-
   networking.firewall.extraCommands = ''
     iptables -A INPUT -p tcp --dport 3240 -s 192.168.122.62 -j ACCEPT
     iptables -A INPUT -p tcp --dport 3240 -j DROP
   '';
-
   boot.kernelModules = [
     "usbip-core"
     "usbip-host"
   ];
-
 }
