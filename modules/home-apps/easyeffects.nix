e019d9996ba5fe9dd21508f65e9d25e2d8667a8c
shibahex <badamsva@gmail.com>
2026-04-29 16:23:37 -0400

    easyeffects: update to latest commit

--- modules/home-apps/easyeffects.nix
+++ modules/home-apps/easyeffects.nix
@@ -4,8 +4,8 @@ let
   easyEffectsProfiles = pkgs.fetchFromGitHub {
     "owner" = "shibahex";
     "repo" = "easyeffects-presets";
-    "rev" = "5dc176fd3061882d58d8c1768f49197a11906973";
-    "hash" = "sha256-2GybT4WcXxiGSakQrE9mhn3Lwj1AH37Fh9F3YZMViPM=";
+    "rev" = "87fe1b742379618272c8934b6962cefcb2a9ed25";
+    "hash" = "sha256-WDk73L/6mpc1EW5hZv1hV964IitE85wZcFBdD7uWL7w=";
   };
 in
 {
