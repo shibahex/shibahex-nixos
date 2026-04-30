b1c512aa9278311369febe2d0a4568075310c6eb
shibahex <badamsva@gmail.com>
2026-04-29 21:43:28 -0400

    ghostty: add themes

--- hosts/nixos-desktop/ghostty.nix
+++ hosts/nixos-desktop/ghostty.nix
@@ -9,11 +9,15 @@ in
       enable = true;
       settings = {
         background = "#000000";
-        custom-shader = "${./shaders/testing.glsl}";
+        background-opacity = 0.95;
+        custom-shader = "${./shaders/retro-glow.glsl}";
+        theme = "Darkside";
       };
     };
+    stylix.targets.ghostty.enable = false;
 
     # puts the .glsl file into ~/.config/ghostty/shaders/
+    xdg.configFile."ghostty/shaders/retro-glow.glsl".source = ./shaders/retro-glow.glsl;
     xdg.configFile."ghostty/shaders/retro-crt.glsl".source = ./shaders/retro-crt.glsl;
     xdg.configFile."ghostty/shaders/testing.glsl".source = ./shaders/testing.glsl;
   };
