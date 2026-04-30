a8006be6db322722cce8e97d81324337a74462e6
shibahex <badamsva@gmail.com>
2026-04-29 20:09:40 -0400

    add unzip

--- modules/applications/programming.nix
+++ modules/applications/programming.nix
@@ -1,15 +1,11 @@
 { pkgs, ... }:
 {
-
   environment.systemPackages = with pkgs; [
-    #Nushell is a terminal
-    nushell
-    arduino
-
     lazygit # for committing on github
     git
+    unzip
   ];
+
   # for Mason LSPs
   programs.nix-ld.enable = true;
-
 }
