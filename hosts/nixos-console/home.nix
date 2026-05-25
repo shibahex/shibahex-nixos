76b3dc4eb2b9ee9e3a91fe376d85e3c8a7dbff2f
shibahex <badamsva@gmail.com>
2026-05-25 16:55:41 -0400

    chore: sync config

--- hosts/nixos-console/home.nix
+++ hosts/nixos-console/home.nix
@@ -1,7 +1,31 @@
-{ pkgs, xboxTheme, ... }:
+{ pkgs, ... }:
 let
-  pegasus = import ./services/pegasus-frontend.nix { inherit pkgs; };
+  xboxTheme = pkgs.fetchFromGitHub {
+    owner = "luxureousproductions-bit";
+    repo = "XboxOSv2";
+    rev = "master";
+    hash = "sha256-C3sCpqNFlhof8fZXv77YyGRmOFK06pJ4SJowx2lHg6k=";
+  };
+  gameOSTheme = pkgs.fetchFromGitHub {
+    owner = "PlayingKarrde";
+    repo = "gameOS";
+    rev = "master";
+    hash = "sha256-EBpIe0aw1FO7DzB6F3oAWD5FRLF2iZGtOHllMxuamdc=";
+  };
+  VapourTheme = pkgs.fetchFromGitHub {
+    owner = "ZagonAb";
+    repo = "Vapour-Pegasus";
+    rev = "master";
+    hash = "sha256-7AZD3w7SyLqG3qmaT9gowPe9TVHSilnXRT6sDr7IMgM=";
+  };
 in
 {
-  home.file.".config/pegasus-frontend/themes/XboxOSv2".source = pegasus.xboxTheme;
+  home.file.".config/pegasus-frontend/themes/XboxOSv2" = {
+    source = xboxTheme;
+    recursive = true;
+  };
+  #   home.file.".config/pegasus-frontend/themes/gameOS" = {
+  #     source = gameOSTheme;
+  #     recursive = true;
+  #   };
 }
