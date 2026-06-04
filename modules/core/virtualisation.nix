978a60d671efe804aaf0e849cb26d3cba67658d2
shibahex <badamsva@gmail.com>
2026-06-04 16:28:38 -0400

    refactor: make virtualisation an option

--- modules/core/virtualisation.nix
+++ modules/core/virtualisation.nix
@@ -1,28 +1,45 @@
-{ pkgs, ... }:
+{ pkgs
+, lib
+, config
+, username
+, ...
+}:
+
+let
+  cfg = config.modules.virtualisation;
+in
 {
-  # libvirtd = { enable = true; };
-  environment.systemPackages = with pkgs; [
-    virt-manager
-    virt-viewer
-    looking-glass-client
-  ];
-  virtualisation = {
-    libvirtd = {
-      enable = true;
-      qemu = {
-        swtpm.enable = true; # Enable software TPM
+  options.modules.virtualisation = {
+    enable = lib.mkEnableOption "virtualisation support";
+    swtpm = lib.mkEnableOption "software TPM support";
+  };
+
+  config = lib.mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [
+      virt-manager
+      virt-viewer
+      looking-glass-client
+    ];
+
+    virtualisation = {
+      libvirtd = {
+        enable = true;
+        qemu = {
+          swtpm.enable = cfg.swtpm;
+        };
       };
     };
-  };
 
-  users.users."qemu-libvirtd" = {
-    extraGroups = [
-      "render"
-      "video"
-      "kvm"
+    users.users."qemu-libvirtd" = {
+      extraGroups = [
+        "render"
+        "video"
+        "kvm"
+      ];
+    };
+
+    systemd.tmpfiles.rules = [
+      "f /dev/shm/looking-glass 0660 ${username} kvm -"
     ];
   };
-  # Looking glass
-  systemd.tmpfiles.rules = [ "f /dev/shm/looking-glass 0660 $USER kvm -" ];
-
 }
