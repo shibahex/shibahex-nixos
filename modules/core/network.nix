b73917d91f6af2d0a1550730af679c52cafd9279
shibahex <badamsva@gmail.com>
2026-06-19 17:44:22 -0400

    feat(network): remove default firewall rules

--- modules/core/network.nix
+++ modules/core/network.nix
@@ -1,8 +1,7 @@
-{
-  pkgs,
-  host,
-  options,
-  ...
+{ pkgs
+, host
+, options
+, ...
 }:
 # let inherit (import ../../hosts/${host}/variables.nix) hostId;
 # in
@@ -14,18 +13,6 @@
     timeServers = options.networking.timeServers.default ++ [ "pool.ntp.org" ];
     firewall = {
       enable = true;
-      allowedTCPPorts = [
-        22
-        80
-        443
-        59010
-        59011
-        8080
-      ];
-      allowedUDPPorts = [
-        59010
-        59011
-      ];
     };
   };
 
